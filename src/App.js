c188ac2ec25bad257c0b7e9e4412a660d7ffde2c
philval <526473+gb96@users.noreply.github.com>
2019-02-26 16:30:05 +0000

    working API call and display events listing, baby CSS

--- src/App.js
+++ src/App.js
@@ -3,37 +3,45 @@ import Title from './components/functional/Title';
 import Searchbox from './components/functional/Searchbox';
 import EventResults from './components/functional/EventResults';
 
+// TODO what happens if user submits "" for an event, currently get all events back.
+// TODO add error message if keyword does not match any events
+
 class App extends Component {
   state = {
     searchValue: '',
     events: null
-}
+  }
 
-//TODO handle null results from api like supernature
-handleSearchSubmit = (event) => {
+  handleSearchSubmit = (event) => {
     event.preventDefault();
     fetch(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=GB&apikey=uzE9qNVGG7cDi4BnIfRyD94V1F1xmyNg&city=Manchester&keyword=${this.state.searchValue}`)
-    .then(response => response.json())
-    .then(({_embedded: {events}}) => {      
-      this.setState({events});
-    })
-    .catch(error => console.log(error))
+      .then(response => response.json())
+      .then((data) => {
 
-}
+        if (data.page.totalElements === 0) {
+          this.setState({ events: null })
+        } else {
+          const eventsObj = data._embedded.events;
+          this.setState({ events: eventsObj });
+        }
+      })
+      .catch(error => console.log(error))
 
-handleChange = (event) => {        
-    this.setState({searchValue: event.target.value})
-}
+  }
 
-  render() {    
-    const {events} = this.state;
+  handleChange = (event) => {
+    this.setState({ searchValue: event.target.value })
+  }
+
+  render() {
+    const { events } = this.state;
     return (
       <div className="App">
-       <Title/>
-       <Searchbox handleSearchSubmit={ this.handleSearchSubmit} handleChange={this.handleChange}/>
-       {/*  Passing down this.state.events as props to eventresults component */}
-       {events && <EventResults events={events}/>}
-        
+        <Title />
+        <Searchbox handleSearchSubmit={this.handleSearchSubmit} handleChange={this.handleChange} />
+        {/*  Passing down this.state.events as props to eventresults component */}
+        {events && <EventResults events={events} />}
+
 
       </div>
     );
--- src/components/functional/SingleEvent.jsx
+++ src/components/functional/SingleEvent.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-const SingleEvent = ({event: {name, type, url, dates: {start: {localDate, localTime}}, _embedded: {venues}}}) => {
+// TODO format date, time
+
+const SingleEvent = ({ event: { name, url, dates: { start: { localDate, localTime } }, _embedded: { venues } } }) => {
     return (
-        <li>
+        <li className="singleEvent">
             <p>{name}</p>
-            <p>{type}</p>
-            <p>{url}</p>
-            <p>{localDate} {localTime}</p>
+            <p><a href={url} target="_blank" alt={name}>Buy Tickets</a></p>
+            <p>Date : {localDate}</p>
+            <p>Time : {localTime}</p>
+            <p>{venues[0].address.line1}</p>
+            <p>{venues[0].city.name}</p>
             <p>{venues[0].postalCode}</p>
-        </li>
+        </li >
     )
 }
 
