087dc689692e19767bf610ed0f86a6435899490d
philval <526473+gb96@users.noreply.github.com>
2019-02-27 17:01:14 +0000

    implemented map, map blobs, CSS grid

--- src/components/functional/EventMap.jsx
+++ src/components/functional/EventMap.jsx
@@ -1,23 +1,23 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Map as LeafletMap, TileLayer, Marker } from 'react-leaflet';
 import EventPopup from './EventPopup';
 
 //TODO display default map of manchester
 
 class EventMap extends Component {
-    state = {
-        lat: 53.4808,
-        lng: -2.2426,
-        zoom: 13,
-      }
+  state = {
+    lat: 53.4808,
+    lng: -2.2426,
+    zoom: 13,
+  }
 
-  render() {     
+  render() {
     const position = [this.state.lat, this.state.lng]
     const zoom = this.state.zoom;
     const eventsArr = this.props.events;
-    
 
-    return (      
+
+    return (
       <LeafletMap
         center={position}
         zoom={zoom}
@@ -32,22 +32,18 @@ class EventMap extends Component {
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        {/* <Marker position={position}>
-          <EventPopup/>
-        </Marker> */}
         {
           eventsArr.map(singleEvent => {
-            return (<Marker key={singleEvent.id} position={   
-              [singleEvent._embedded.venues[0].location.latitude, singleEvent._embedded.venues[0].location.longitude]
-          }>
-            <EventPopup/>
-          </Marker>)   
-        })
-      }
+            return (
+              <Marker key={singleEvent.id} position={
+                [singleEvent._embedded.venues[0].location.latitude, singleEvent._embedded.venues[0].location.longitude]
+              }>
+                <EventPopup singleEvent={singleEvent} />
+              </Marker>
+            )
+          })
+        }
 
-        {/* <Marker position={[51.5074, -0.1278]}>
-          <EventPopup/>
-        </Marker> */}
       </LeafletMap>
     );
   }
--- src/components/functional/EventPopup.jsx
+++ src/components/functional/EventPopup.jsx
@@ -1,11 +1,18 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { Popup } from 'react-leaflet';
 
-const EventPopup = () => {
+const EventPopup = ({ singleEvent: { name, url, dates: { start: { localDate, localTime } }, _embedded: { venues } } }) => {
+
     return (
         <Fragment>
             <Popup>
-                Popup for any custom information.
+                <p>{name}</p>
+                <p><a href={url} target="_blank" alt={name}>Buy Tickets</a></p>
+                <p>Date : {localDate}</p>
+                <p>Time : {localTime}</p>
+                <p>{venues[0].address.line1}</p>
+                <p>{venues[0].city.name}</p>
+                <p>{venues[0].postalCode}</p>
             </Popup>
         </Fragment>
     )
